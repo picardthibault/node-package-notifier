933a1293a3a73562dd76f9dc3162b421d3a3bf3d
Thibault Picard <picardthibault@gmail.com>
2024-01-31 08:22:12 +0100

    Update some dependencies

--- webpack.plugins.ts
+++ webpack.plugins.ts
@@ -2,16 +2,17 @@ import type IForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const ForkTsCheckerWebpackPlugin: typeof IForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const CopyPlugin = require('copy-webpack-plugin');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 
-export const rendererPlugins = [
-  new ForkTsCheckerWebpackPlugin({
-    logger: 'webpack-infrastructure',
-  }),
-];
+const tsCheckerWebpackPlugin = new ForkTsCheckerWebpackPlugin({
+  logger: 'webpack-infrastructure',
+});
+
+export const rendererPlugins = [tsCheckerWebpackPlugin];
 
 export const mainPlugins = [
+  tsCheckerWebpackPlugin,
   new CopyPlugin({
     patterns: [{ from: 'src/main/ressources', to: 'ressources' }],
   }),

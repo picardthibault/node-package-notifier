3f66dd4cbc93cccce681f5ab63bfa3aa5e07f7d7
Thibault Picard <picardthibault@gmail.com>
2024-05-03 08:18:25 +0200

    Handle back mouse button on package details view

--- src/renderer/views/packages/PackageDetails.tsx
+++ src/renderer/views/packages/PackageDetails.tsx
@@ -13,12 +13,21 @@ import {
 } from '@renderer/stores/PackageDetailsStore';
 import { MenuStore, menuStore } from '@renderer/stores/MenuStore';
 import { EyeOutlined } from '@ant-design/icons';
+import { navigateTo } from '@renderer/effects/MenuEffect';
 
 interface TableItemType {
   key: number;
   tagName: string;
   tagVersion: string;
 }
+
+const backMouseButtonListener: (to: string) => (event: MouseEvent) => void =
+  (to: string) => (event: MouseEvent) => {
+    if (event.button === 3) {
+      void navigateTo(to);
+    }
+    event.preventDefault();
+  };
 
 const PackageDetails: FunctionComponent = () => {
   const { t } = useTranslation();
@@ -36,6 +45,15 @@ const PackageDetails: FunctionComponent = () => {
   const [tags, setTags] = useState<TableItemType[]>([]);
 
   const [formInstance] = Form.useForm();
+
+  useEffect(() => {
+    const listener = backMouseButtonListener(previousSelectedKey);
+    window.addEventListener('mouseup', listener);
+
+    return () => {
+      window.removeEventListener('mouseup', listener);
+    };
+  });
 
   useEffect(() => {
     setIsLoading(true);
